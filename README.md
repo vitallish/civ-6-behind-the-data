1feac17f17ba2696f4d42473f0af46f53f25ce4a
Gogs <gogs@fake.local>
2018-02-20 23:48:46 -0500

    added watch module for file watching

---
+++ README.md
@@ -0,0 +1,1 @@
+# Civ VI Turn Logger
--- main.js
+++ main.js
@@ -27,8 +27,8 @@ function createWindow () {
     // when you should delete the corresponding element.
     win = null
   })
+  
 }
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -53,3 +53,10 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+var chokidar = require('chokidar');
+
+// One-liner for current directory, ignores .dotfiles
+chokidar.watch('.', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+  console.log(event, path);
+});
