1c67cfbd0db858241efa72014503e23f9239f21f
Vitaly Druker <vdruker@gmail.com>
2018-02-28 00:33:29 -0500

    started work with sequalize.

--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 ### sqlite3
 
 - See answer: https://stackoverflow.com/a/47531692/1812425
-- You may need to install the following globally with admin privilages:
-	npm install --global --production windows-build-tools
-	npm install -g node-gyp
+- You may need to install the following globally with admin privileges:
+```
+npm install --global --production windows-build-tools
+npm install -g node-gyp
+```
 - I then had to install python27 manually (make sure to add to path)
 - restart bash
 - make sure there are no spaces in your file path
--- js/model.js
+++ js/model.js
@@ -2,22 +2,68 @@ console.log('whooooo');
 const path = require('path')
 const {app, BrowserWindow} = require('electron')
 
-var sqlite3 = require('sqlite3').verbose();
-var db_path = path.join(app.getPath("userData"), "info.db")
-console.log(db_path)
-var db = new sqlite3.Database(db_path);
+// var sqlite3 = require('sqlite3');
+const db_path = path.join(app.getPath("userData"), "info.sqlite");
 
-db.serialize(function () {
-    db.run("CREATE TABLE lorem (info TEXT)");   
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', null, null, {
+    host: 'localhost',
+    dialect: 'sqlite',
 
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
 
-    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-        console.log(row.id + ": " + row.info);
-    });
+    // SQLite only
+    storage: db_path
 });
-db.close();
+
+const Employee = sequelize.define('employee', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        get() {
+            const title = this.getDataValue('title');
+            // 'this' allows you to access attributes of the instance
+            return this.getDataValue('name') + ' (' + title + ')';
+        },
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        set(val) {
+            this.setDataValue('title', val.toUpperCase());
+        }
+    }
+});
+// force: true not supported for sqlite?
+sequelize.sync()
+
+return sequelize.transaction(function (t) {
+
+    // chain all your queries here. make sure you return them.
+    return Employee
+        .create({ name: 'John Doe', title: 'senior engineer' }
+            , {transaction: t});
+
+}).then(function (result) {
+    // Transaction has been committed
+    // result is whatever the result of the promise chain returned to the transaction callback
+    console.log("it worked")
+}).catch(function (err) {
+    // Transaction has been rolled back
+    // err is whatever rejected the promise chain returned to the transaction callback
+    console.log(err)
+});
+
+//sequelize.sync()
+//
+// Employee
+//     .create({ name: 'John Doe', title: 'senior engineer' })
+//     .then(employee => {
+//         console.log(employee.get('name')); // John Doe (SENIOR ENGINEER)
+//         console.log(employee.get('title')); // SENIOR ENGINEER
+//     })
